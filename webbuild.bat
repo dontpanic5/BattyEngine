ff8179793fa3a7385340071e4fd56d083ee98cf3
frenchfries <frenchfries55boi@gmail.com>
2024-07-09 22:02:53 -0400

    Add audio mgr

--- webbuild.bat
+++ webbuild.bat
@@ -1,4 +1,5 @@
 call emcc -c -Iraylib\src src\AnimationMgr.cpp -Os -Wall -DPLATFORM_WEB
+call emcc -c -Iraylib\src src\AudioMgr.cpp -Os -Wall -DPLATFORM_WEB
 call emcc -c -Iraylib\src src\BattyEngine.cpp -Os -Wall -DPLATFORM_WEB
 call emcc -c -Iraylib\src src\ControllerMgr.cpp -Os -Wall -DPLATFORM_WEB
 call emcc -c -Iraylib\src src\Entity.cpp -Os -Wall -DPLATFORM_WEB
@@ -9,4 +10,5 @@ call emcc -c -Iraylib\src src\Ground.cpp -Os -Wall -DPLATFORM_WEB
 call emcc -c -Iraylib\src src\LevelMgr.cpp -Os -Wall -DPLATFORM_WEB
 call emcc -c -Iraylib\src src\Ui.cpp -Os -Wall -DPLATFORM_WEB
 call emcc -c -Iraylib\src src\BattyUtils.cpp -Os -Wall -DPLATFORM_WEB
-emar rcs build\battyenginelib.a AnimationMgr.o BattyEngine.o ControllerMgr.o Entity.o EntityMgr.o EnvironmentalObject.o GameCamera.o Ground.o LevelMgr.o Ui.o BattyUtils.o
+emar rcs build\battyenginelib.a AnimationMgr.o AudioMgr.o 
+BattyEngine.o ControllerMgr.o Entity.o EntityMgr.o EnvironmentalObject.o GameCamera.o Ground.o LevelMgr.o Ui.o BattyUtils.o
