f321df3ae25c5b472578a0ba468649ba10970127
LAPTOP-CDI097KP\n8 <frenchfries55boi@gmail.com>
2026-02-18 12:59:06 -0700

    Continued model viewer.

--- src/screen_model_viewer.cpp
+++ src/screen_model_viewer.cpp
@@ -1,11 +1,19 @@
 #include "screen_model_viewer.h"
+#include "BattyEngine.h"
+#include "AnimationMgr.h"
 
 void InitModelViewerScreen()
 {
-
+	const char* path = modelViewerPath;
+	const char** pPath = &path;
+	AnimationMgr::Instance().LoadMultipleModelAnimations(pPath, 1);
 }
 
 void UpdateModelViewerScreen()
 {
 
+}
+
+void DrawModelViewerScreen()
+{
 }
